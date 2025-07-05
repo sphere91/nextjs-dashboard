4fa207fb9275a12d5ba0c09e292f5a877ed77e1a
sphere91 <wmans7@gmail.com>
2025-07-05 12:38:30 +0200

    Added metadata to all pages: /login page. /dashboard/ page. /dashboard/customers page. /dashboard/invoices/create page. /dashboard/invoices/[id]/edit page.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+    description: 'Manage your customers here',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
 }
