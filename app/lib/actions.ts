076ffda938c7f1be79e255e9f28845f17eecc90a
sphere91 <wmans7@gmail.com>
2025-07-05 12:03:54 +0200

    fixed linting errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,7 +103,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        return { message: 'Database Error: Failed to Update Invoice.' + error };
     }
 
     revalidatePath('/dashboard/invoices');
